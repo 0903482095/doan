e9ce678ab82ef09e0dd030b2abdaaaf36c0a9664
0903482095 <090348209>
2019-11-09 16:52:16 +0700

    update

--- src/main/java/com/hoangnt/service/impl/StadiumServiceImpl.java
+++ src/main/java/com/hoangnt/service/impl/StadiumServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hoangnt.entity.Shift;
 import com.hoangnt.entity.StatusShift;
 import com.hoangnt.model.InformationUser;
 import com.hoangnt.model.ShiftDTO;
@@ -18,14 +19,13 @@ import com.hoangnt.utils.Status;
 import com.hoangnt.utils.TypeStadium;
 
 @Service
-public class StadiumServiceImpl implements StadiumService{
+public class StadiumServiceImpl implements StadiumService {
 
 	@Autowired
 	StadiumRepository stadiumRepository;
-	
+
 	@Autowired
 	StatusShiftRepository statusShiftRepository;
-
 
 	@Override
 	public List<StadiumDTO> getByIdAddress(int id) {
@@ -53,7 +53,7 @@ public class StadiumServiceImpl implements StadiumService{
 	}
 
 	@Override
-	public List<StadiumDTO> getFullByIdAddress(int id,String date) {
+	public List<StadiumDTO> getFullByIdAddress(int id, String date) {
 		List<StadiumDTO> stadiumDTOs = new ArrayList<StadiumDTO>();
 		stadiumRepository.getByIdAddress(id).forEach(stadium -> {
 			StadiumDTO stadiumDTO = new StadiumDTO();
@@ -92,6 +92,47 @@ public class StadiumServiceImpl implements StadiumService{
 				}
 			});
 
+			if (statusShiftResponses.isEmpty()) {
+				stadium.getShifts().forEach(shift -> {
+					StatusShiftResponse statusShiftResponse = new StatusShiftResponse();
+
+					ShiftDTO shiftDTO = new ShiftDTO();
+					shiftDTO.setId(shift.getId());
+					shiftDTO.setName(shift.getName());
+					shiftDTO.setCash(shift.getCash());
+
+					statusShiftResponse.setShiftDTO(shiftDTO);
+					statusShiftResponse.setStatus(0);
+
+					statusShiftResponses.add(statusShiftResponse);
+
+				});
+
+			} else {
+				for (int i = 0; i < stadium.getShifts().size(); i++) {
+					Shift shift = stadium.getShifts().get(i);
+					boolean check = true;
+					for (int j = 0; j < statusShiftResponses.size(); j++) {
+						if (statusShiftResponses.get(j).getShiftDTO().getId() == shift.getId()) {
+							check = false;
+						}
+					}
+					if (check) {
+
+						StatusShiftResponse statusShiftResponse = new StatusShiftResponse();
+						ShiftDTO shiftDTO = new ShiftDTO();
+						shiftDTO.setId(shift.getId());
+						shiftDTO.setName(shift.getName());
+						shiftDTO.setCash(shift.getCash());
+
+						statusShiftResponse.setShiftDTO(shiftDTO);
+						statusShiftResponse.setStatus(0);
+
+						statusShiftResponses.add(statusShiftResponse);
+					}
+				}
+			}
+
 			stadiumDTO.setStatusShiftResponses(statusShiftResponses);
 			stadiumDTOs.add(stadiumDTO);
 		});
@@ -101,7 +142,7 @@ public class StadiumServiceImpl implements StadiumService{
 	@Override
 	public void deleteStadium(int id) {
 		stadiumRepository.deleteById(id);
-		
+
 	}
 
 }
